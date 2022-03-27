bcb55bcb22dbf83e0f7fa8e9fa18bb9d75c95938
Caroline McKenna <cmckenna@smith.edu>
2022-03-27 15:44:46 -0400

    adding initial documentation

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Data for movies in the Criterion Collection as of March 2022
+#'
+#' 1,515 items in the Criterion Collection, including box sets, from
+#' <https://www.criterion.com/shop/browse/list>.
+#'
+#' @format A data frame of 1,515 rows with each row representing an item, such
+#' as a movie or box set, in the Criterion Collection.
+#' \describe{
+#'   \item{Rank}{Spine number of the movie or box set}
+#'   \item{Year}{Year in which the movie was released}
+#'   \item{Country}{Country in which the movie was made}
+#'   \item{Title}{Movie or box set title}
+#'   \item{Director}{Director of the movie}
+#' }
+"criterion"
