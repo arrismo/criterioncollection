83c99166758505ccd474a6028610c0d8b07a3cf7
Arris Moise <mikaelmoise00@gmail.com>
2022-03-27 22:26:28 -0400

    added test for rows and columns

---
+++ tests/testthat/test-df.R
@@ -0,0 +1,12 @@
+
+library(testthat)
+library(CriterionCollection)
+
+test_that("check number of rows", {
+  expect_equal(nrow(criterion), 1515)
+})
+
+
+test_that("check number of columns", {
+  expect_equal(ncol(criterion), 5)
+})
