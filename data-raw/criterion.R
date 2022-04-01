69ba9349f5e5f6fbd6cf0219b4a65bf125c5f9f4
Arris Moise <mikaelmoise00@gmail.com>
2022-04-01 17:28:26 -0400

    attempting to remove \n from country column

--- data-raw/criterion.R
+++ data-raw/criterion.R
@@ -2,9 +2,11 @@
 
 
 library(readr)
-library(janitor)
+library(stringr)
 
 criterion <- read_csv("data-raw/criterion.csv")
+criterion$country <- gsub("[\r\n]", "", criterion$country)
+
 
 usethis::use_data(criterion, overwrite = TRUE)
 
