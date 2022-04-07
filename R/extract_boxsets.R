8c10d6302f409942b4fe6c2c34ad1195ec2b678a
Arris Moise <mikaelmoise00@gmail.com>
2022-04-06 22:56:51 -0400

    writing extract_boxsets function

---
+++ R/extract_boxsets.R
@@ -0,0 +1,13 @@
+
+# logic behind extract_bosets function
+# if row is missing year, country, and director
+# then that is a boxset
+
+
+for (i in colnames(criterion)){
+  for (j in rownames(criterion)){
+    as.Text(print(criterion[, j]))
+  }
+}
+
+
