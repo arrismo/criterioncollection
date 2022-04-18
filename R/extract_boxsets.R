ce963d22b7af64dfd9298ca8e51cf5efe2fb4ab5
Arris Moise <mikaelmoise00@gmail.com>
2022-04-18 14:23:21 -0400

    removing boxsets_function, leaving just the dataset, and updating the description file

--- R/extract_boxsets.R
+++ R/extract_boxsets.R
@@ -1,14 +1,14 @@
-globalVariables(c(
-  "py_run_file"
-))
-
-#' Calls the parse API to load boxset dataframe
-#' Adds boxsets data set to your environment
-#' @import Matrix
-#' @import reticulate
-#' @export
-extract_boxsets <- function() {
-  py_run_file("inst/parse_hub_api.py")
-  }
+#' globalVariables(c(
+#'   "py_run_file"
+#' ))
+#'
+#' #' Calls the parse API to load boxset dataframe
+#' #' Adds boxsets data set to your environment
+#' #' @import Matrix
+#' #' @import reticulate
+#' #' @export
+#' extract_boxsets <- function() {
+#'   py_run_file("inst/parse_hub_api.py")
+#'   }
 
 
