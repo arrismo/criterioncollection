80160491f6cea4b40f1fac40ad7b529c52d73a20
Caroline McKenna <cmckenna@smith.edu>
2022-04-28 10:25:28 -0400

    maybe fixing things

--- data-raw/sticker/sticker.R
+++ data-raw/sticker/sticker.R
@@ -1,6 +1,7 @@
-library(hexSticker)
+#library(hexSticker)
+#library(magick)
 
-criterion_logo <- image_read("https://upload.wikimedia.org/wikipedia/commons/5/5d/The_Criterion_Collection_Logo.svg")
+#criterion_logo <- magick::image_read("https://upload.wikimedia.org/wikipedia/commons/5/5d/The_Criterion_Collection_Logo.svg")
 
-sticker(criterion_logo, package = "criterioncollection", p_size = 20, s_x = 1,
-        s_y = .75, s_width = .6, filename = "sticker/criterion_hex.png")
+#sticker(criterion_logo, package = "criterioncollection", p_size = 20, s_x = 1,
+#        s_y = .75, s_width = .6, filename = "sticker/criterion_hex.png")
