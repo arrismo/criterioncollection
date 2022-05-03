d812cec5b9d8acdc19a672c5d90ac375b6ae10b5
Caroline McKenna <cmckenna@smith.edu>
2022-05-03 13:51:47 -0400

    slight sticker changes

--- man/figures/sticker.R
+++ man/figures/sticker.R
@@ -3,6 +3,6 @@ library(magick)
 
 imgurl <- "./man/figures/criterion-logo.png"
 
-sticker(imgurl, package = "criterioncollection", p_size = 5, s_x = 1,
-        s_y = .75, s_width = .5, h_fill = "#000000", h_color = "#A0A0A0",
+sticker(imgurl, package = "criterioncollection", p_size = 5, p_y = 1, s_x = 1,
+        s_y = 1, s_width = .6, h_fill = "#000000", h_color = "#A0A0A0",
         filename = "man/figures/criterion_hex.png")
